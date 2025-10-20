f492ee412c696f5e55748132fc3ee68fe25fc7b0
miguelarmesto <miguel.armestod@udc.es>
2025-10-20 12:11:04 +0200

    Solving errors

--- backend/user-service/.venv/main.py
+++ backend/user-service/.venv/main.py
@@ -4,7 +4,7 @@ from typing import Dict, List
 from uuid import uuid4
 import threading
 
-# --- Modelos ---
+
 class UserCreate(BaseModel):
     username: str
 
@@ -12,19 +12,15 @@ class User(BaseModel):
     id: str
     username: str
 
-# --- App principal ---
 app = FastAPI(title="User Service", version="0.1")
 
-# --- Almacenamiento temporal en memoria ---
+
 users: Dict[str, User] = {}
 lock = threading.Lock()
 
-# --- Endpoints ---
 @app.post("/users", response_model=User, status_code=201)
 def create_user(payload: UserCreate):
-    """
-    Crea un usuario si el nombre de usuario no existe.
-    """
+    
     with lock:
         if any(u.username == payload.username for u in users.values()):
             raise HTTPException(status_code=400, detail="Username already exists")
@@ -35,12 +31,12 @@ def create_user(payload: UserCreate):
 
 @app.get("/users", response_model=List[User])
 def list_users():
-    """Lista todos los usuarios"""
+    
     return list(users.values())
 
 @app.get("/users/{user_id}", response_model=User)
 def get_user(user_id: str):
-    """Devuelve un usuario por su id(404 si no existe)"""
+    
     user = users.get(user_id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
@@ -48,5 +44,5 @@ def get_user(user_id: str):
 
 @app.get("/health")
 def health():
-    """Endpoint para comprobar que el servicio está corriendo"""
+    
     return {"status": "ok"}
